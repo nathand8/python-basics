75eeb639c5145840914146f998509b8ac12af79f
Nathan Davis <nathand120@gmail.com>
2021-09-17 12:09:42 -0600

    Added Variables and Strings test, observation, and toFill files

---
+++ exercises/0_Variables/Test.py
@@ -0,0 +1,34 @@
+###
+### Don't make edits to this file!
+### Edit ToFill.py instead!
+###
+
+import ToFill as toFill
+
+score = 0
+
+if(hasattr(toFill, 'doSimpleCalculations') and callable(getattr(toFill, 'doSimpleCalculations'))):
+    if toFill.doSimpleCalculations() == 9:
+        score+=1
+else:
+    print("ATTN:!!!Please create method doSimpleCalculations!!!")
+
+if(hasattr(toFill, 'combineStringsSimple') and callable(getattr(toFill, 'combineStringsSimple'))):
+    if toFill.combineStringsSimple() == "University of Utah Rules" :
+        score+=1
+else:
+    print("ATTN:!!!Please create method combineStringsSimple!!!")
+
+if(hasattr(toFill, 'combineNumsStrings') and callable(getattr(toFill, 'combineNumsStrings'))):
+    if toFill.combineNumsStrings() == "University of Utah has 32000 students":
+        score+=1
+else:
+    print("ATTN:!!!Please create method combineNumsStrings!!!")
+
+if(hasattr(toFill, 'convertStringToNum') and callable(getattr(toFill, 'convertStringToNum'))):
+    if toFill.convertStringToNum(2500)  == 50 and toFill.convertStringToNum("3000")  == 60:
+        score+=1
+else:
+    print("ATTN:!!!Please create method convertStringToNum!!!")
+
+print("Your score is:", score, "out of 5")
---
+++ exercises/0_Variables/ToFill.py
@@ -0,0 +1,28 @@
+def doSimpleCalculations():
+    #  create variable equal to 99999
+    #  create variable equal to 11111
+    return 9#  return first variable divided by the second one
+
+
+def combineStringsSimple():
+  #  create variable Univerisity of
+  #  create variable Utah
+  #  create variable Rules
+  return ()#   return University of Utah Rules
+
+
+def combineNumsStrings():
+  #  create number variable 32000
+  #  create variable University of Utah has
+  #  create variable students
+  return ()#  combine all of the variables so you would get University of Utah has 32000 students
+
+
+#create a function named convertStringToNum, that will have one input parameter
+#take that input parameter, convert it to float and divide by 50
+#return the result
+
+
+
+if __name__ == "__main__":
+  print("Don't run this file directly. Test it by running Test.py!")
