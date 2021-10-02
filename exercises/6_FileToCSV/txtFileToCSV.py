2077291c3b056be1165d5141059541e14ae11dc5
Nathan Davis <nathand120@gmail.com>
2021-10-02 07:31:11 -0600

    Added text to csv challenge

---
+++ exercises/6_FileToCSV/txtFileToCSV.py
@@ -0,0 +1,17 @@
+# The Challenge: 
+#   Transform the file Mkt_data.txt to a 
+#   CSV format like in "CorrectAnswerScreenshot.png"
+#
+# Your steps to get there: (The don't have to be done in this order)
+# - Create a header line with following columns: Time,Bid\Ask,Price,Volume. NOTE NO SPACES
+# - Remove all of the timestamp lines, i.e ======== Data: .....
+# - Remove the 1900-01-01 from the timestamp but leave the time itself
+# - Get rid of all spaces and empty lines
+# - Replace 0 or 1 in the second position with Bid or Ask, Bid for 0, Ask for 1
+# - Save the header line and propely formatted lines to the COMMA-SEPARATED csv file named market_data.csv
+#
+# To check your answer, open "market_data.csv" in Excel and compare it to the Screenshot.
+
+
+
+
